3f24b69c4c0ac8d02b26ee4552d8d1f73029f65d
winaboy <chuan_he@icloud.com>
2014-12-03 19:21:50 -0600

    better GUI

--- startHere.py
+++ startHere.py
@@ -1,4 +1,5 @@
 from Tkinter import *
+import tkMessageBox
 from tkFileDialog import askopenfilename
 
 import numpy
@@ -7,118 +8,117 @@ import os
 
 class Application(Frame):
 
-    def __init__(self, master):
+    def __init__(self, master, **options):
         # Initialize the Frame
-        self.instructionLabelRow = 0
-        self.instructionLabelCol = 10
-        self.instructionLabelSpan = 1
+        Frame.__init__(self, master, options)
+        self.padx = 10
+        self.pady = 10
 
-        self.messageLabelRow = 1
-        self.messageLabelCol = 10
-        self.messageLabelSpan = 1
-
-        self.browseButtonRow = 2
-        self.browseButtonCol = 10
-
-        self.urlLabelRow = 3
-        self.urlLabelCol = 10
-        self.urlLabelSpan = 1
-
-        self.executeButtonRow = 4
-        self.executeButtonCol = 10
-
-        self.paddingX = 5
-        self.paddingY = 10
-
-
-        Frame.__init__(self, master)
         self.grid()
-        self.url = NO
-        self.create_widgets()
 
-    def create_widgets(self):
+        self.url = NO
+        self.create_widgets(master)
+
+        self.pwd = os.getcwd()
+
+    def create_widgets(self, master):
+        #Label(master, text="First").grid(row=0)
+        #Label(master, text="Second").grid(row=1)
+
+        #e1 = Entry(master)
+        #e2 = Entry(master)
+
+        #e1.grid(row=0, column=1)
+        #e2.grid(row=1, column=1)
+
         # Create Instruction Label
-        self.instructionLabel = Label(self, text = "Please Click on 'Browse File' to Select a File", font = "Helvetica 16 bold")
-        self.instructionLabel.grid(padx = self.paddingX, pady = self.paddingY, columnspan = self.instructionLabelSpan)
+        self.instructionLabel = Label(self, \
+            text = "Please click \"Specify Hole\" to select the hole from mesh\nOr browse the curve and calculate area", \
+            font = "Arial 10", justify = LEFT, height = 3, bg = "white")
+        self.instructionLabel.grid(row = 0, column = 0, columnspan = 2, padx = self.padx, pady = self.pady)
+
+        self.drawLineButton = Button(self, text = "Specify Hole", \
+            font = ("Arial Black", 16), \
+            command = self.start3DViewer, width = 12, height = 2)
+        self.drawLineButton.grid(row = 1, column = 0, padx = self.padx, pady = self.pady)
+
+        
+        # Create Browser Button
+        self.browseButton = Button(self, text = "Browse File", \
+            font = ("Arial Black", 16), \
+            command = self.openFile, width = 12, height = 2)
+        self.browseButton.grid(row = 1, column = 1, padx = self.padx, pady = self.pady)
+
+        self.areaButton = Button(self, text = "Get Area", \
+            font = ("Arial Black", 16), \
+            command = self.runScripts, width = 12, height = 2)
+        self.areaButton.grid(row = 2, column = 1, padx = self.padx, pady = self.pady)
 
         # Create Message Label
-        self.messageLabel = Label(self, text = "Message Will Be Displayed here", fg = "green", height = 4, font = "Helvetica 14")
-        self.messageLabel.grid(padx = self.paddingX, pady = 5, columnspan = self.messageLabelSpan)
+        self.messageLabel = Label(self, text = "Status: wait for user input", height = 4, font = "Helvetica 8", bg = "white")
+        self.messageLabel.grid(row = 5, column = 0, columnspan = 2, sticky = W)
 
-        # Create Browser Button
-        self.browseButton = Button(self, text = "Browse File", command = self.openFile, justify = CENTER, width = 16, height = 4)
-        self.browseButton.grid(padx = self.paddingX, pady = self.paddingY)
-
-        # Create Url Label
-        self.urlLabel = Message(self, text = "No File Selected yet", width = 300)
-        self.urlLabel.grid(padx = 10)
-
-        # Create Execute Button
-        self.executeButton = Button(self, text = "Execute", command = self.run3DViewer)
-        self.executeButton.grid(padx = self.paddingX, pady = self.paddingY)
-
-        self.areaButton = Button(self, text = "Get Area", command = self.getArea)
-        self.areaButton.grid(padx = self.paddingX, pady = self.paddingY)
-
-        self.drawLineButton = Button(self, text = "Specify Hole", command = self.startDrawLineSoftware)
-        self.drawLineButton.grid(padx = self.paddingX, pady = self.paddingY)
 
 
     def openFile(self):
         filename = askopenfilename()
-        self.urlLabel.configure(text = filename)
-        self.url = YES
+        self.url = filename
+        self.messageLabel.configure(text = "Status: " + filename + " is been selected")
 
-    def run3DViewer(self):
-        pwd = os.getcwd()
+    def runScripts(self):
+        if not self.url:
+            tkMessageBox.showwarning(
+            "Error",
+            "Please select a valid \".curve \" first."
+            )
+            return 
 
-        self.messageLabel.configure(text = "Now Circling the hole")
-        # run 3D viewer
-        filename = pwd + '\\meshdrawing\\MeshDrawing.exe'
-        os.system(filename)        
+        if not self.url.endswith(".curve"):
+            tkMessageBox.showwarning(
+            "Error", 
+            "\"" + self.url + " \" is not a valid \".curve \" first."
+            )
+            return 
+
 
         #run connecting script
-        self.messageLabel.configure(text = "Now Connecting the curves")
-        ply = self.urlLabel.cget("text")
-        curve = ply[:-4] + ".curve"
-        filename = 'python ' + pwd + '\\scripts\\findHole.py ' + curve
+        self.messageLabel.configure(text = "Status: now connecting the curves")
+        curve = self.url
+        filename = 'python ' + self.pwd + '\\scripts\\findHole.py ' + curve
         os.system(filename)
 
         #run TriMultPoly
-        self.messageLabel.configure(text = "Now Triangulate the curve")
+        self.messageLabel.configure(text = "Status: now triangulate the curve")
         cCurve = curve[:-6] + ".continous"
-        filename = pwd + '\\TriMultPoly\\TMP.exe ' + cCurve + ' 1 1 0 0 1 0 1 0'
-        print filename
+        filename = self.pwd + '\\TriMultPoly\\TMP.exe ' + cCurve + ' 1 1 0 0 1 0 1 0'
         os.system(filename)
 
-        self.messageLabel.configure(text = "Finished")
+        #get Area
+        dynObj = cCurve + "_dyn.obj"
+        area = self.getArea(dynObj)
 
-    def run3DViewer2(self):
-        if self.url:
-            self.messageLabel.configure(text = "Now Executing")
+        tkMessageBox.showwarning(
+        "Area", 
+        "The hole area is " + str(area)
+        )
 
-            #print self.urlLabel.cget("text")
-        else:
-            self.messageLabel.configure(text = "Please Select a File First")
+        self.messageLabel.configure(text = "Stauts: finished")
 
-    def getArea(self):
-        fileObj = open(self.urlLabel["text"], 'r')
-        # print fileObj
+    def getArea(self, url):
+        lines = open(url, 'r')
+
         pointList = []
-        self.sum = 0
+        area = 0
 
-        for line in fileObj:
+        for line in lines:
             brokeDown = line.split()
             if brokeDown[0] == "v":
                 pointList.append(brokeDown)
             if brokeDown[0] == "f":
-                # print brokeDown
-                # print pointList
-
                 subArea = self.areaForTriangle(pointList[int(brokeDown[1])-1], pointList[int(brokeDown[2])-1], pointList[int(brokeDown[3])-1])
-                self.sum += float(subArea)
+                area += float(subArea)
 
-        self.messageLabel.configure(text = self.sum)
+        return area
 
     def areaForTriangle(self, p1, p2, p3):
         ary1 = numpy.array((float(p1[1]), float(p1[2]), float(p1[3])))
@@ -133,17 +133,24 @@ class Application(Frame):
 
         return (s*(s-d1)*(s-d2)*(s-d3)) ** 0.5
 
-    def startDrawLineSoftware(self):
-        self.messageLabel.configure(text = "Draw line to Specify Area")
+    def start3DViewer(self):
+        filename = self.pwd + '\\meshdrawing\\MeshDrawing.exe'
+        os.system(filename)
+
 
 
 
 root = Tk()
+root.resizable(False, False)
 root.title("Skull Holes Interface")
-root.geometry("350x400")
-# root.config(height = 700, width = 400)
 
-app = Application(root)
+root.update()
+scnWidth = root.winfo_screenwidth()
+scnHeight = root.winfo_screenheight()
+tmpcnf = '%dx%d+%d+%d' %(420 , 320, (scnWidth-420)/2, (scnHeight-320)/2)
+root.geometry(tmpcnf)
+
+app = Application(root, bg = 'white')
 
 root.mainloop()
 
